2d163290ed88558bcc74a4e4b994cc391f89d624
jubx20 <69223133+jubx20@users.noreply.github.com>
2021-01-18 14:55:53 -0300

    Create i_rodape.php

---
+++ PHP/i_rodape.php
@@ -0,0 +1,9 @@
+	<div id="copyright" class="container">
+		<!--Sla se essa parte é necessaria
+		<p>&copy; Untitled. All rights reserved. | Photos by <a href="http://fotogrph.com/">Fotogrph</a> | Design by <a href="http://templated.co" rel="nofollow">TEMPLATED</a>.</p>
+		-->
+		<p>Inclusão e acessibilidade: mapeamento de rotas acessíveis para pessoas com deficiência</p>
+		<p>Isabelle Galvão | Gabriela Fernandes | Jullia Nunes</p>
+	</div>
+</body>
+</html>
