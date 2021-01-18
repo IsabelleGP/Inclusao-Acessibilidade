f5c068c05fee70eb64f397eca532dd698dbf74b4
jubx20 <69223133+jubx20@users.noreply.github.com>
2021-01-18 14:55:51 -0300

    Create i_menu.php

---
+++ PHP/i_menu.php
@@ -0,0 +1,22 @@
+
+<div id="wrapper">
+	<div id="menu-wrapper">
+		<div id="menu" class="container">
+			<ul>
+				<li><a href="../PHP/index.php">Mapeamento</a></li>
+				<li><a href="../PHP/sobre.php">Sobre</a></li>
+				<li><a href="../PHP/desenvolvimento.php">Desenvolvimento</a></li>
+				<li><a href="../PHP/pesquisa.php">Pesquisa</a></li>
+				<li><a href="../PHP/experienciapratica.php">Experiência Prárica</a></li>
+
+			</ul>
+		</div>
+
+	<div id="header-wrapper">
+		<div id="header" class="container">
+			<div id="logo">
+				<h1><a href="#">Inclusão e Acessibilidade</a></h1>
+			</div>
+		</div>
+	</div>
+	</div>
