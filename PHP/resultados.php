8c65e150fb63363ca60e318bf433d722be65732c
jubx20 <69223133+jubx20@users.noreply.github.com>
2021-01-18 14:56:10 -0300

    Create resultados.php

---
+++ PHP/resultados.php
@@ -0,0 +1,53 @@
+    <?php
+		include ("i_topo.php");
+	?>
+    <?php
+		include ("i_menu.php");
+	?>
+		<center>
+	<div id="page" class="container">
+		<div id="content">
+			<div class="title">
+				<h2>Resultados Esperados</h2>
+			</div>
+			<p>Através da aplicação os deficientes poderão ter mais segurança aos se locomoverem aos seus destinos, utilizando trajetos que se adequam ao seu tipo de deficiência, para que dessa forma sua integridade física possa ser conservada ao mesmo tempo em que conseguem mais inclusão na sociedade. Além disso o site servirá para informar as pessoas tudo sobre as deficiências, desde os seus tipos até suas dificuldades diárias e limitações, ressaltando a importância de fazer com que os deficientes se sintam inseridos na sociedade.</p>
+	</div>
+</div>
+	<div id="page" class="container">
+		<div id="content">
+			<div class="title">
+				<h2>Diagrama de caso de uso</h2>
+				<br>
+				<br>
+				<br>
+				<br>
+				<img src="../imagens/casodeuso.png" alt="Diagrama de caso de uso">
+			</div>
+	</div>
+</div>
+		</center>
+		
+	<div id="footer-wrapper">
+		<div id="footer" class="container">
+			<div id="box2">
+				<div class="title">
+					<h2>bibliografia</h2>
+				</div>
+				<ul class="style1">
+					<li><a href="http://www.scielo.br/pdf/ean/v10n3/v10n3a19.pdf" target="blank">Mapeamento da acessibilidade do portador de limitação física a serviços básicos de saúde</a></li>
+					<li><a href="https://www.scielosp.org/scielo.php?pid=S1413-81232008000300027&script=sci_arttext&tlng=pt#back" target="blank">Acessibilidade a benefícios legais disponíveis no Rio de Janeiro para portadores de deficiência física</a></li>
+					<li><a href="https://periodicos.ufsm.br/educacaoespecial/article/view/4415" target="blank">A acessibilidade e a educação: um direito constitucional como base para um direito social da pessoa com deficiência</a></li>
+					<li><a href="http://www.periodicos.usp.br/actafisiatrica/article/view/102875/101168" target="blank">A história da deficiência, da marginalização à inclusão social: uma mudança de paradigma</a></li>
+					<li><a href="https://periodicosonline.uems.br/index.php/interfaces/article/view/548/512" target="blank">Acessibilidade das pessoas com deficiência física</a></li>
+					<li><a href="http://www.ibdd.org.br/" target="blank">Instituto Brasileiro dos Direitos da Pessoa com Deficiência</a></li>
+				</ul>
+			</div>
+		</div>
+	</div>
+	
+	</div>
+</div>
+
+    <?php
+		include ("i_rodape.php");
+	?>
