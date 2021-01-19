23343dccf5c9e699a79c05a7ea300771c98a0efb
susuhh <54377207+susuhh@users.noreply.github.com>
2021-01-19 10:21:33 -0300

    Create desenvolvimento.php
    reorganizando arquivos do projeto

---
+++ desenvolvimento.php
@@ -0,0 +1,47 @@
+    <?php
+		include ("i_topo.php");
+	?>
+    <?php
+		include ("i_menu.php");
+	?>
+	<!--<div id="banner"></div>-->
+	<center>
+	<div id="page" class="container">
+		<div id="content">
+			<div class="title">
+				<h2>Ideia inicial</h2>
+			</div>
+			<p>A ideia inicial deste projeto era montar um protótipo de um aplicativo que fosse atender os deficientes físicos, facilitando seu trajeto assim que informados sua localização atual e seu destino, mostrando um caminho mais viável que visa dar seguridade á sua saúde física, para que eles não encontrem no caminho alguns objetos aéreos, meio-fio extremamente alto ou ausência de piso tátil. Os resultados esperados foram baseados nesta ideia, juntamente com a produção deste site, cujo intuito é informar, conscientizar e serve também com uma ferramenta de inclusão social.</p>
+		</div>
+		<div id="content">
+			<div class="title">
+				<h2>Início do desenvolvimento</h2>
+			</div>
+			<p>A primeira etapa de produção do projeto é através de pesquisas em artigos científicos, jornais publicados em sites, livros e em quaisquer outros materiais que tratam sobre o assunto, com o intuito de explicar de maneira detalhada e exemplificada o tema escolhido, fazendo com que ele seja compreendido e reconhecido por quem tiver acesso.</p>
+			<p>Para a melhor compreensão do projeto os integrantes do grupo fizeram uma reunião com a Fabiana Bonilha.</p>
+			<p>Fabiana Bonilha é deficiente visual e pesquisadora do Centro Nacional de Tecnologia Assistiva (CNRTA), que é um eixo de acessibilidade e gestão de pesquisa. A partir de questões que foram organizadas pelas integrantes do grupo ela pode contribuir para o desenvolvimento e o produto final do projeto. Ela orientou sobre o que se pode ter no aplicativo citando as maiores dificuldades encontradas por uma pessoa com deficiência, como por exemplo falta de piso tátil, obstáculos aéreos e falta de sinalização de trânsito sonora, além de orientar os integrantes a fazer um site acessível para deficientes visuais e auditivos.</p>
+		</div>
+		<div id="content">
+			<div class="title">
+				<h2>Problemas encontrados na realização do projeto</h2>
+			</div>
+			<p>Como todo projeto em desenvolvimento, os desenvolvedores encontram diversos problemas em definir o tema, encontra em outros trabalhos acadêmicos algo que eles possam usar, entre outro. É evidente que sem que estes problemas sejam resolvidos, o projeto pode não prosseguir do  modo desejado, e foi por este motivo que os desenvolvedores deste projeto buscaram se aprofundar mais no assunto e entender o real problem.</p>
+			<p>No início era complicada a busca pelos materiais que iriam ser desenvolvidos, entretanto, esta busca foi facilitada com o auxílio de um orientador. Não foi difícil escolher um orientador, devido ao fato de que o grupo já possuía contato com o  atual orientador, cuja função é revisar e ajudar a complementar o material do projeto.</p>
+			<p>Pode-se concluir que a realização de um projeto é complicada, no entanto, o resultado é satisfatório.</p>
+		</div>
+		<div id="content">
+			<div class="title">
+				<h2>Documentos do projeto</h2>
+			</div>
+			<p><a href="../PHP/objetivos.php">Objetivos</a></p>
+			<p><a href="../PHP/metodologia.php">Metodologia</a></p>
+			<p><a href="../PHP/fundamentacao.php">Fundamentação teórica</a></p>
+			<p><a href="../PHP/resultados.php">Resultados Esperados</a></p>
+		</div>
+	</div>
+		</center>
+</div>
+	
+    <?php
+		include ("i_rodape.php");
+	?>
