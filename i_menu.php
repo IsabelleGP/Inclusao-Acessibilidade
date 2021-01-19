6b42ba5737d0c28847fa5b76f4126a8d1cdf355d
IsabelleGP <69484978+IsabelleGP@users.noreply.github.com>
2021-01-19 10:48:22 -0300

    Update i_menu.php
    Alterado: incluido cadastro no menu

--- i_menu.php
+++ i_menu.php
@@ -7,8 +7,8 @@
 				<li><a href="../PHP/sobre.php">Sobre</a></li>
 				<li><a href="../PHP/desenvolvimento.php">Desenvolvimento</a></li>
 				<li><a href="../PHP/pesquisa.php">Pesquisa</a></li>
-				<li><a href="../PHP/experienciapratica.php">Experiência Prárica</a></li>
-
+				<li><a href="../PHP/experienciapratica.php">Experiência Prática</a></li>
+				<li><a href="../PHP/cadastro.php">Cadastro de Usuários</a></li>
 			</ul>
 		</div>
 
