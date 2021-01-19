f9217ff1ad7bd1f0142a1f3a7fd639918217786f
susuhh <54377207+susuhh@users.noreply.github.com>
2021-01-19 10:21:52 -0300

    Create metodologia.php

---
+++ metodologia.php
@@ -0,0 +1,47 @@
+    <?php
+		include ("i_topo.php");
+	?>
+    <?php
+		include ("i_menu.php");
+	?>
+	<center>	
+	<div id="page" class="container">
+		<div id="content">
+			<div class="title">
+				<h2>Metodologia</h2>
+			</div>
+			<h3>Revisão bibliográfica</h3>
+			<p>A primeira etapa do desenvolvimento do projeto são as pesquisas em artigos científicos, jornais publicados em sites, livros e outros materiais com o objetivo de explicar detalhadamente o tema e as razões de ele ter sido escolhido, para que desta forma ele e sua importância  possam ser compreendidos. Após essa fase a segunda e terceira etapas serão baseadas nessas mesmas pesquisas.</p>
+			<h3>Desenvolvimento de um software</h3>
+			<p>A segunda etapa do projeto, é o desenvolvimento de um software que será criado utilizando linguagens de programação, como PHP, C, Java, JavaScript e MySQL. Esse aplicativo contará com a construção de um local para o usuário se cadastrar, e também um mapeamento cartográfico de alguns lugares da  cidade de Campinas para que, desta forma, o usuário possa selecionar uma rota partindo do local onde ele se encontra e o lugar que pretende ir, e o aplicativo consiga lhe mostrar uma rota que garanta segurança ao deficiente e dos aparelhos que ele utiliza para auxiliar na sua locomoção.</p>
+			<h3>Desenvolvimento de um site</h3>
+			<p>Muitas pessoas não têm conhecimento sobre conteúdo no qual este projeto é baseado, e isso é ruim porque segundo o Instituto Brasileiro de Geografia e Estatística (IBGE), o número  de pessoas com alguma deficiência física no Brasil fica em torno de 45,6 milhões de pessoas, que corresponde a 24% da população brasileira, e o descaso com as necessidades dessas pessoas cresce diariamente, e por isso será também efetuada a criação de um site informativo. Por esta razão a terceira e última parte será a criação de um site para que cada vez mais pessoas tenham acesso a informações como essas, o site será desenvolvido utilizando linguagens de programação como HTML, CSS, JQuery, PHP e Bootstrap.</p>
+			<p>Este site não existirá somente para poder conscientizar melhor as pessoas, solidificando com argumentos, que é necessário atender as necessidades desta parte da sociedade, e que as dificuldades que um deficiente físico enfrenta ao ter que fazer atividades do dia-a-dia, como por exemplo atravessar uma rua, quando  uma calçada não possui "baixo relevo" facilitando a descida dos cadeirantes, não são irrelevantes, assim como também mais informações sobre as deficiências, como por exemplo os tipos de deficiência física e mais sobre a deficiência visual. Mas também para ajudar essas pessoas que possuem deficiência a ter sua voz ouvida, podendo deixar suas reclamações e sugestões de melhorias para os trajetos que ainda não são acessíveis para elas.</p>
+	</div>
+</div>
+	</center>
+	
+	<div id="footer-wrapper">
+		<div id="footer" class="container">
+			<div id="box2">
+				<div class="title">
+					<h2>bibliografia</h2>
+				</div>
+				<ul class="style1">
+					<li><a href="http://www.scielo.br/pdf/ean/v10n3/v10n3a19.pdf" target="blank">Mapeamento da acessibilidade do portador de limitação física a serviços básicos de saúde</a></li>
+					<li><a href="https://www.scielosp.org/scielo.php?pid=S1413-81232008000300027&script=sci_arttext&tlng=pt#back" target="blank">Acessibilidade a benefícios legais disponíveis no Rio de Janeiro para portadores de deficiência física</a></li>
+					<li><a href="https://periodicos.ufsm.br/educacaoespecial/article/view/4415" target="blank">A acessibilidade e a educação: um direito constitucional como base para um direito social da pessoa com deficiência</a></li>
+					<li><a href="http://www.periodicos.usp.br/actafisiatrica/article/view/102875/101168" target="blank">A história da deficiência, da marginalização à inclusão social: uma mudança de paradigma</a></li>
+					<li><a href="https://periodicosonline.uems.br/index.php/interfaces/article/view/548/512" target="blank">Acessibilidade das pessoas com deficiência física</a></li>
+					<li><a href="http://www.ibdd.org.br/" target="blank">Instituto Brasileiro dos Direitos da Pessoa com Deficiência</a></li>
+				</ul>
+			</div>
+		</div>
+	</div>
+	
+	</div>
+</div>
+
+    <?php
+		include ("i_rodape.php");
+	?>
