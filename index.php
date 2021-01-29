8f69dd6544285ac8d9f22510d39ddd8e1e8a2a13
susuhh <54377207+susuhh@users.noreply.github.com>
2021-01-29 10:32:38 -0300

    Update index.php

--- index.php
+++ index.php
@@ -23,7 +23,7 @@
 
 			var mymap = L.map('mapid').setView([-22.9064, -47.0616], 13);
 
-			L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+			L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1Ijoic3VzYW5mcmFuY2FsYXNzaSIsImEiOiJja2tpYm16bXgwbzd2MnVxOW5reWw4ZXdwIn0.9lJuJEeTkzelKqiO_VBxxg', {
 				maxZoom: 18,
 				attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
 					'<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
