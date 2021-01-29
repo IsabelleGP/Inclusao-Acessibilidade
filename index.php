0f4e273fb21f01c12463a9ff10f9e355e4cb44c6
IsabelleGP <69484978+IsabelleGP@users.noreply.github.com>
2021-01-29 10:09:37 -0300

    a
    a

--- index.php
+++ index.php
@@ -2,17 +2,30 @@
 
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
+
+
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link rel="stylesheet" href="https://unpkg.com/leaflet@1.5.1/dist/leaflet.css" integrity="sha512-xwE/Az9zrjBIphAcBb3F6JVqxf46+CDLwfLMHloNu6KEQCAWi6HcDUbeOfBIptF7tcCzusKFjFw2yuvEpDL9wQ==" crossorigin=""/>
 <script src="https://unpkg.com/leaflet@1.5.1/dist/leaflet.js" integrity="sha512-GffPMF3RvMeYyc1LWMHtK8EbPv0iNZ8/oTtHPx9/cc2ILxQ+u905qIwdpULaqDkyBKgOaB57QTMg7ztg8Jm2Og==" crossorigin=""></script>
+
+<!--<div id="mapid">
+	<link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css"
+	   integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A=="
+	   crossorigin=""/>
+	<!-- Make sure you put this AFTER Leaflet's CSS 
+	 <script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js"
+	   integrity="sha512-XQoYMqMTK8LvdxXYG3nZ448hOEQiglfqkJs1NOQV44cWnUrBc8PkAOcXy20w0vlaXaVUearIOBhiXZ5V3ynxwA=="
+	   crossorigin=""></script>
+</div>-->
+
 <title>Inclusão e Acessibilidade</title>
 <meta name="keywords" content="" />
 <meta name="description" content="" />
 <link href="http://fonts.googleapis.com/css?family=Raleway:400,200,500,600,700,800,300" rel="stylesheet" />
 <link href="../default.css" rel="stylesheet" type="text/css" media="all" />
 <link href="../fonts.css" rel="stylesheet" type="text/css" media="all" />
-<!--<link rel="stylesheet" href="css/index.css"> -->
+
 </head>
 <body>
     <?php
